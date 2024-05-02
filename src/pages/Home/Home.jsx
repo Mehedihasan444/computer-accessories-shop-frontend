d6a1666788b315d44c4c97add9ba59ea64d92784
Mehedi Hasan <mehedihasan67705251@gmail.com>
2024-05-03 01:15:42 +0600

    fixed feature section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import Product_Card from "../../components/Product_Card/Product_Card";
-import Banner from "../../components/home_banner/Banner";
-import SecondaryBanner from "../../components/home_banner/SecondaryBanner";
+import Banner from "../../components/banner/Banner";
 import Categories from "../../components/categories/Categories";
 import Features from "../../components/Features/Features";
 import Discount from "../../components/Discount/Discount";
@@ -14,7 +13,6 @@ const Home = () => {
   return (
     <div>
       <Banner />
-      <SecondaryBanner />
       <Features />
       <Deal_Of_the_Week/>
       <Home_Page_Products />
